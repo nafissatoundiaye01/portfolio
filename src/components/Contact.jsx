ed9fe59182fc558c323ed4a8add17aff38f26a1c
nafi <nafissatound1@gmail.com>
2024-11-15 10:58:04 +0000

    final touch

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,10 +64,12 @@ const Contact = () => {
   };
 
   return (
-    <div className="mt-12 flex flex-row gap-10">
+    <div
+      className="mt-12 flex flex-col lg:flex-row gap-10"
+    >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.55] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.55] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Contactez-moi</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -75,45 +77,45 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Votre nom</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Votre nom</span>
             <input
-              type='text'
-              name='name'
+              type="text"
+              name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="Quel est votre nom complet ?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Votre email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Votre email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               onChange={handleChange}
               placeholder="Quelle est votre adresse email ?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Votre message</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Votre message</span>
             <textarea
               rows={7}
-              name='message'
+              name="message"
               value={form.message}
               onChange={handleChange}
               placeholder="Que voulez-vous m'addresser ?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
 
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
             {loading ? "Envoi..." : "Envoyer"}
           </button>
@@ -122,7 +124,7 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='flex-[0.45] h-auto'
+        className="flex-[0.45] h-auto"
       >
         <ServerCanvas />
       </motion.div>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,7 @@ const ProjectCard = ({
   tags,
   image,
   realisation,
-  video_link,  
+  video_link,
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -27,7 +27,7 @@ const ProjectCard = ({
       {/* Modal pour video */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="relative w-[80%] max-w-[800px] h-[450px] p-5 bg-white rounded-lg flex flex-col justify-center items-center">
+          <div className="relative w-[90%] max-w-[800px] h-[60%] max-h-[450px] p-5 bg-white rounded-lg flex flex-col justify-center items-center">
             <button
               onClick={closeModal}
               className="absolute top-2 right-2 text-[red] text-2xl"
@@ -37,7 +37,7 @@ const ProjectCard = ({
             <video
               className="w-full h-full rounded-lg"
               controls
-              controlsList="nodownload" 
+              controlsList="nodownload"
               src={video_link}
             >
               Votre navigateur ne prend pas en charge la lecture de vidéos.
@@ -53,16 +53,16 @@ const ProjectCard = ({
             scale: 1,
             speed: 450,
           }}
-          className="bg-tertiary p-5 rounded-2xl sm:w-[350px] w-full"
+          className="bg-tertiary p-5 rounded-2xl w-full max-w-[350px]"
         >
           <div
-            className="relative w-full h-[230px]"
-            onClick={openModal} 
+            className="relative w-full h-56 sm:h-60 md:h-64 lg:h-72"
+            onClick={openModal}
           >
             <img
               src={image}
               alt="project_image"
-              className="w-full h-full object-cover rounded-2xl cursor-pointer" 
+              className="w-full h-full object-cover rounded-2xl cursor-pointer"
             />
 
             <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
@@ -77,10 +77,16 @@ const ProjectCard = ({
           </div>
 
           <div className="mt-5">
-            <h3 className="text-white font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px]">{description}</p>
-            <p className="mt-2 text-secondary text-[14px]"><strong>{date}</strong></p>
-            <p className="mt-2 text-secondary text-[14px]">
+            <h3 className="text-white font-bold text-lg sm:text-xl lg:text-2xl">
+              {name}
+            </h3>
+            <p className="mt-2 text-secondary text-sm sm:text-base">
+              {description}
+            </p>
+            <p className="mt-2 text-secondary text-sm sm:text-base">
+              <strong>{date}</strong>
+            </p>
+            <p className="mt-2 text-secondary text-sm sm:text-base">
               <strong>Participation : </strong>
               {realisation}
             </p>
@@ -88,7 +94,7 @@ const ProjectCard = ({
 
           <div className="mt-4 flex flex-wrap gap-2">
             {tags.map((tag) => (
-              <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+              <p key={`${name}-${tag.name}`} className={`text-sm ${tag.color}`}>
                 #{tag.name}
               </p>
             ))}
@@ -98,8 +104,6 @@ const ProjectCard = ({
     </>
   );
 };
-
-
 
 const Works = () => {
   return (
@@ -112,7 +116,7 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-sm sm:text-base md:text-lg max-w-3xl leading-relaxed"
         >
           Les projets présentés ci-dessous illustrent mes compétences et mon expertise à travers des exemples pratiques de mon travail. Ils démontrent ma capacité à résoudre des défis complexes, à utiliser diverses technologies et à gérer efficacement des projets.
         </motion.p>
